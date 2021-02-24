9afda38851e0ecbb40d2b4f692a14b5e2136f999
Jf_Dess <noobshippuden13@gmail.com>
2021-02-24 20:34:10 +0000

    iniciando o Css

--- public/application/config/config.php
+++ public/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://https://8001-scarlet-chameleon-ss1y6yhw.ws-us03.gitpod.io/';
+$config['base_url'] = 'https://8001-scarlet-chameleon-ss1y6yhw.ws-us03.gitpod.io/';
 
 /*
 |--------------------------------------------------------------------------
--- public/application/views/Home.php
+++ public/application/views/Home.php
@@ -6,9 +6,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?= base_url() ?>assets/style.css">
 	<title>Home-Site P.W. III</title>
 </head>
 <body>
-	<h1>Wallcume</h1>
+	<div class="principal_Title">
+		<h1 class="Title_Font">Seja Bem vindo</h1>
+		<h5 class="SubTitle_Font">Aqui iremos fazer um site com base em php e no Code Ignitor</h5>
+	</div>
 </body>
 </html>
