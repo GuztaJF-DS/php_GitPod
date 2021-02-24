3bff0944833947934b867b7dea854855cf67f288
Jf_Dess <noobshippuden13@gmail.com>
2021-02-24 18:41:09 +0000

    code igniter configurado

---
+++ public/application/views/Home.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home-Site P.W. III</title>
+</head>
+<body>
+	<h1>Wallcume</h1>
+</body>
+</html>
