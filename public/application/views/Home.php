f6b2c6fa2be0a6322d7afe3766fe33dc5ed573c4
Jf_Dess <noobshippuden13@gmail.com>
2021-03-03 19:32:14 +0000

    Continuando do Design

--- public/application/views/Home.php
+++ public/application/views/Home.php
@@ -7,12 +7,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="<?= base_url() ?>assets/style.css">
-	<title>Home-Site P.W. III</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<title>Sistemas Operacionais:Windows</title>
 </head>
-<body>
+<body style="background: rgb(165,27,11);
+background: linear-gradient(180deg, rgba(165,27,11,1) 0%, rgba(98,12,3,1) 100%);">
 	<div class="principal_Title">
-		<h1 class="Title_Font">Seja Bem vindo</h1>
-		<h5 class="SubTitle_Font">Aqui iremos fazer um site com base em php e no Code Ignitor</h5>
+		<h1 class="Title_Font">Sistemas Operacionais</h1>
+		<h5 class="SubTitle_Font">Windows</h5>
 	</div>
+	<div class="Links">
+		<a class="Link" href="">Linux</a>
+		<a class="Link" href="">Mac Os</a>
+	</div>
+	<div class="Pages">
+		<div class="col-md-9 col-sm-5 Page_info"><h2>Windows 1</h2>O Windows 1.0 foi onde tudo começou, lançado em 1985. Até sua chegada, os sistemas operacionais funcionavam apenas em modo texto e este foi o primeiro a oferecer uma interface gráfica em cima do modo texto (MS-DOS) de maneira que qualquer pessoa pudesse interagir com o sistema sem precisar digitar comandos no terminal. Para os padrões de hoje ele pode parecer feio, simples e antiquado, mas saiba que foi extremamente importante por introduzir ao mundo conceitos que hoje são completamente normais como menus que caem ao serem clicados, a utilização de uma seta para apontar e clicar em elementos, barras de rolagem e as janelas.</div>
+		<div class="col-md-3 col-sm-5 Page_info"><h2>Windows 2</h2>Se você reparar bem, as janelas da interface do Windows 1.0 não se sobrepunham. O Windows 2.0 chegou em 1987 trazendo essa funcionalidade, o que provocou um processo por parte da Apple alegando que a Microsoft havia copiado o sistema da empresa da Maçã - a Apple perdeu a briga. Capaz de rodar sem um disco rígido, o 2.0 se tornou mais popular do que o antecessor.</div>
+		<div class="col-md-6 col-sm-5 Page_info"><h2>Windows 3</h2>Foi com o Windows 3 que o sistema realmente começou a decolar e ganhar espaço. Lançado em 1990, ele veio com uma interface reformulada, delineando o padrão visual que convivemos por muitos anos até a chegada do Windows 8. Além do visual renovado, ele trouxe diversas melhorias como suporte a drives de CD e placas de som, além de vir pré-instalado em discos rígidos, ocupando "monstruosos" 5 MB. Além disso, o Windows 3.0 introduziu softwares importantes como o Bloco de Notas, um editor de texto e o clássico Paciência.</div>
+	</div>
+	<div class="Pages">
+		<div class="col-md-5 col-sm-10 Page_info"><h2>Windows 95</h2>Lançado em 1995, o sistema foi um marco na história da Microsoft, do próprio Windows e, quem sabe, até da humanidade. A interface gráfica, mais refinada do que nunca, praticamente não mudou visualmente até a chegada do Windows XP e em comportamento até o Windows 8 em 2012. Nesta versão, a Microsoft introduziu o Windows Explorer e a barra de tarefas, presentes até os dias de hoje e que não devem desaparecer tão cedo. Importante notar o Internet Explorer na captura de tela. No entanto, ele não vinha instalado por padrão, mas era possível adquiri-lo em um pacote de atualização chamado Plus.</div>
+		<div class="col-md-5 col-sm-10 Page_info"><h2>Windows 98</h2>O Windows 98 foi lançado em 1998, como o nome sugere, e trouxe vários avanços em relação ao 95, apesar de não ter sofrido mudanças visuais significativas. Importante mencionar o suporte a dispositivos USB e múltiplos monitores e a chegada do Internet Explorar já instalado por padrão - foi aí quando começou a derrocada do Netscape. No ano seguinte, a Microsoft lançou o Windows 98 Second Edition (SE), que corrigiu vários bugs da versão anterior.</div>
+	</div>
+	<div class="Pages">
+		<div class="col-10 Page_info"><h2>Windows XP</h2> o sistema fez história e ainda é um dos mais utilizados no mundo, atrás apenas do Windows 7 e Windows 8, e isso somente porque a empresa descontinuou o suporte técnico a essa versão recentemente, Sua característica mais marcante, à primeira vista, foi o visual caprichado, com cores vivas, a montanha verde com céu azulado, a barra de tarefas azul e os demais elementos que marcaram uma nova era no design do sistema. Além disso, unido aos avanços de hardware e softwares de terceiros, tornou-se uma versão extremamente confiável para as mais variadas atividades e, hoje, 14 anos após seu lançamento, ainda temos milhões de estabelecimentos rodando o XP em seus computadores.
+		</div>
+	</div>
+	<div class="Pages">
+		<div class="col-md-5 col-sm-10 Page_info"><h2>Windows 7</h2>Após o fracasso do Windows Vista, a empresa lançou o Windows 7, que veio para salvar a pátria. Depois do XP, o 7 foi o sistema da "zona de conforto" dos usuários, já que oferecia um visual bem agradável e desempenho e confiabilidade nos mesmos níveis do Windows XP. Com o encerramento do suporte à versão antiga, o Windows 7 passou a ser o sistema operacional mais utilizado do mundo e atualmente conta com pouco mais de 60% de participação no mercado. </div>
+		<div class="col-md-5 col-sm-10 Page_info"><h2>Windows 8</h2>Tudo ia bem com o Windows 7, mas, em 2012, a Microsoft colocou os pés pelas mãos em uma aposta que não deu muito certo: eliminou o Menu Iniciar e empurrou goela abaixo dos usuários os "Live Tiles", quadrados vivos que continham programas e exibiam informações relevantes em tempo real, em uma interface voltada para dispositivos com tela sensível ao toque.O problema é que quase ninguém possui uma tela do tipo e a maioria já está acostumada a trabalhar bem com os bons e velhos teclado e mouse. Além disso, a falta do Menu Iniciar deixou muita gente perdida, o que contribuiu para a alta taxa de adoção do Windows 7. Resultado: o Windows 8 foi um fiasco.</div>
+	</div>
+
 </body>
 </html>
